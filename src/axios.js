dcc2f12337d14ca55e0c9226af70b7741a23709b
Daniel <danielwebdeveloper220@gmail.com>
2020-10-29 17:21:14 -0400

    deploying to firebase

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
 import axios from "axios";
 
-dotenv.config();
-
 const instance = axios.create({
-  baseURL: process.env.DB_TINDER_CLONE,
+  baseURL: process.env.REACT_APP_DB_TINDER_CLONE,
 });
 
 export default instance;
